f31fe27a2d642ef8e4be8acd956d4fb76e838ddd
Daniel Prado <daniel.vascomadagama2@gmail.com>
2023-10-17 11:21:39 -0300

    roteiro 2 finalizado

--- roteiro_1/app.py
+++ roteiro_1/app.py
@@ -2,5 +2,9 @@ import streamlit as st
 
 st.title("Roteiro 1: Primeiros Passos com Streamlit")
 
+title = st.text_input('my team')
 if st.button("Clique-me"):
-    st.write("Você clicou no botão!")
+    st.write('my team is', title)
+
+
+    
--- roteiro_2/app.py
+++ roteiro_2/app.py
@@ -2,6 +2,32 @@ import streamlit as st
 
 st.title("Roteiro 2: Widgets Interativos")
 
-value = st.slider("Escolha um valor", 0, 100)
-if st.button("Mostrar valor do slider"):
-    st.write(f"Valor escolhido: {value}")
+on = st.toggle('Mostrar opções')
+
+if on:
+    st.write('Opções mostradas!')
+
+
+    value = st.slider("Escolha um valor", 0, 100)
+    agree = st.checkbox('Eu concordo')
+    times = st.radio(
+        "Qual seu time favorito?",
+        [":rainbow[fluminense]", "***VASCO***", "flamengo"],
+        index=None,
+    )
+
+    opções = st.selectbox(
+        'Qual time é o melhor?',
+        ('Vasco', 'Flamengo', 'Fluminense'))
+
+
+
+    if st.button("Mostrar valores"):
+        st.write(f"Valor escolhido: {value}")
+
+        if agree:
+            st.write('Ótimo!')
+
+        st.write("Seu time é:", times)
+
+        st.write('o melhor time é:', opções)
